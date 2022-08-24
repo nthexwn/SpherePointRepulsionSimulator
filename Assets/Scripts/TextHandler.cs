f39446b3fa7c108749b1e820284f9b772a734840
nthexwn <nthexwn@gmail.com>
2022-08-24 00:22:55 -0700

    Attempt at quaternion-based implementation

--- Assets/Scripts/TextHandler.cs
+++ Assets/Scripts/TextHandler.cs
@@ -5,31 +5,33 @@ using System.IO;
 // Based on https://support.unity.com/hc/en-us/articles/115000341143-How-do-I-read-and-write-data-from-a-text-file-
 public class TextHandler
 {
-    public static void Print(string str)
+    private FileStream stream;
+    private StreamReader reader;
+    private StreamWriter writer;
+
+    public TextHandler(string path)
+    {
+        stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+        reader = new StreamReader(stream);
+        writer = new StreamWriter(stream);
+    }
+    public string Read()
+    {
+        return reader.ReadToEnd();
+    }
+
+    public void Print(string str)
     {
         Debug.Log(str);
-        WriteString(str);
+        Write(str);
     }
 
-    public static void WriteString(string str)
+    private void Write(string str)
     {
-        // Write the line
-        string path = "Assets/Resources/log.txt";
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine(str);
-        writer.Close();
+        writer.Write(str);
 
         // Re-import the file to update the reference in the editor
-        AssetDatabase.ImportAsset(path);
-        TextAsset asset = (TextAsset)Resources.Load("log");
-    }
-
-    public static string ReadString()
-    {
-        string path = "Assets/Resources/log.txt";
-        StreamReader reader = new StreamReader(path);
-        string str = reader.ReadToEnd();
-        reader.Close();
-        return str;
+        //AssetDatabase.ImportAsset(path);
+        //TextAsset asset = (TextAsset)Resources.Load("log");
     }
 }
